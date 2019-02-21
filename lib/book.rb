6a29d2ce39ac9d019869b1ee6c106b7e28c41aa9
Tyler Hunt <tyhunt04@gmail.com>
2019-02-21 15:37:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,21 @@
 class Book
+  
   def initialize(title)
     @title = title
   end
+  
   def title
     @title
   end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+  
 end
 
 Book.new("And Then There Were None")
